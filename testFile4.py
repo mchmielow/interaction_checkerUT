1a361c49c35b232c5e003c86f6f8b76aa6da5fed
mchmielow <michal.chmielow@gmail.com>
2022-04-28 17:02:52 +0200

    NewFeature4: multiple lines in one file

--- testFile4.py
+++ testFile4.py
@@ -1,4 +1,4 @@
-print("Hello world!")
-print("Hello world!")
+print("Hello world! multiple lines in one commit1")
+print("Hello world! multiple lines in one commit2")
 print("Hello world!")
 print("Hello world!")
